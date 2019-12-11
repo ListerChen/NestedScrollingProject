fb8f1ac36cea9235d1d43d4f71cd4bee2828d0d1
chentianran <chentianran1@xiaomi.com>
2019-12-11 15:41:49 +0800

    [Feature]: 实现parent的滑动拦截以及fling事件向RecyclerView的传递
    Signed-off-by: chentianran <chentianran1@xiaomi.com>

--- app/src/main/java/com/lister/nestedscrolltest/NestedLinearLayout.java
+++ app/src/main/java/com/lister/nestedscrolltest/NestedLinearLayout.java
@@ -3,12 +3,14 @@ package com.lister.nestedscrolltest;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.LinearLayout;
+import android.widget.Scroller;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,12 +24,21 @@ import com.youth.banner.Banner;
 public class NestedLinearLayout extends LinearLayout implements NestedScrollingParent2 {
 
     private int TOUCH_SLOP;
+    private boolean mIsBeingDragged;
+    private int mLastMotionY;
+    private int mLastY;
 
     private Banner mBanner;
     private int mBannerHeight;
     private RecyclerView mRecyclerView;
     private NestedScrollingParentHelper mNestedParentHelper;
+    private VelocityTracker mVelocityTracker;
+    private Scroller mScroller;
     private int mMaxVelocity;
+    /**
+     * 是否已经将 parent 的滑动速率传递给 RecyclerView, 一次事件流只需要传递一次
+     */
+    private boolean mHasFlingFromParentToRV;
 
     public NestedLinearLayout(Context context) {
         this(context, null);
@@ -39,6 +50,7 @@ public class NestedLinearLayout extends LinearLayout implements NestedScrollingP
 
     public NestedLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mScroller = new Scroller(context);
         ViewConfiguration vc = ViewConfiguration.get(context);
         TOUCH_SLOP = vc.getScaledTouchSlop();
         mMaxVelocity = vc.getScaledMaximumFlingVelocity();
@@ -60,7 +72,6 @@ public class NestedLinearLayout extends LinearLayout implements NestedScrollingP
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        Log.e("TAGGG", "onSizeChanged");
         mBannerHeight = mBanner.getMeasuredHeight();
         LayoutParams layoutParams = (LayoutParams) mRecyclerView.getLayoutParams();
         layoutParams.height = getMeasuredHeight() - getContext().getResources()
@@ -97,13 +108,148 @@ public class NestedLinearLayout extends LinearLayout implements NestedScrollingP
     }
 
     @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        int action = ev.getActionMasked();
+        if (action == MotionEvent.ACTION_DOWN) {
+            mHasFlingFromParentToRV = false;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return super.onInterceptTouchEvent(ev);
+        int action = ev.getAction();
+        switch (action & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                mIsBeingDragged = false;
+                mLastMotionY = (int) ev.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int y = (int) ev.getRawY();
+                int diff = Math.abs(mLastMotionY - y);
+                mLastMotionY = y;
+                boolean canRecyclerViewScroll = true;
+                if (mRecyclerView != null) {
+                    canRecyclerViewScroll = mRecyclerView.canScrollVertically(-1);
+                }
+                if (diff > TOUCH_SLOP && !canRecyclerViewScroll
+                        && !isInNestedArea((int) ev.getRawX(), (int) ev.getRawY())) {
+                    mIsBeingDragged = true;
+                    ViewParent parent = getParent();
+                    if (parent != null) {
+                        parent.requestDisallowInterceptTouchEvent(true);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mIsBeingDragged = false;
+                break;
+        }
+        return mIsBeingDragged;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        if (mRecyclerView != null && mRecyclerView.canScrollVertically(-1)) {
+            return true;
+        }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // scroll 处理
+                mLastY = (int) event.getRawY();
+                // fling 处理
+                initOrResetVelocityTracker();
+                resetScroller();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // scroll 处理
+                if (mLastY == 0) {
+                    mLastY = (int) event.getRawY();
+                }
+                int y = (int) event.getRawY();
+                int dy = mLastY - y;
+                mLastY = y;
+                scrollBy(0, dy);
+                // fling 处理
+                initVelocityTrackerIfNotExist();
+                mVelocityTracker.addMovement(event);
+                break;
+            case MotionEvent.ACTION_UP:
+                // scroll 处理
+                mLastY = 0;
+                // fling 处理
+                mVelocityTracker.computeCurrentVelocity(1000, mMaxVelocity);
+                int velocityY = (int) -mVelocityTracker.getYVelocity();
+                recycleVelocityTracker();
+                parentFling(velocityY);
+                break;
+        }
+        return true;
+    }
+
+    private void parentFling(int velocityY) {
+        mScroller.fling(0, getScrollY(), 0, velocityY,
+                0, 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        postInvalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            int curY = mScroller.getCurrY();
+            scrollTo(0, curY);
+            invalidate();
+            if (getScrollY() == curY && !mHasFlingFromParentToRV) {
+                int velocityY = (int) mScroller.getCurrVelocity();
+                if (mRecyclerView != null) {
+                    mRecyclerView.fling(0, velocityY);
+                    mHasFlingFromParentToRV = true;
+                }
+            }
+        }
+    }
+
+    private boolean isInNestedArea(int x, int y) {
+        if (mRecyclerView == null) {
+            return false;
+        }
+        int[] location = new int[2];
+        mRecyclerView.getLocationOnScreen(location);
+        int left = location[0];
+        int top = location[1];
+        int right = left + mRecyclerView.getMeasuredWidth();
+        int bottom = top + mRecyclerView.getMeasuredHeight();
+        if (x >= left && x <= right && y >= top && y <= bottom) {
+            return true;
+        }
+        return false;
+    }
+
+    private void initOrResetVelocityTracker() {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        } else {
+            mVelocityTracker.clear();
+        }
+    }
+
+    private void initVelocityTrackerIfNotExist() {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+    }
+
+    private void recycleVelocityTracker() {
+        if (mVelocityTracker != null) {
+            mVelocityTracker.recycle();
+            mVelocityTracker = null;
+        }
+    }
+
+    private void resetScroller() {
+        if (mScroller != null && !mScroller.isFinished()) {
+            mScroller.abortAnimation();
+        }
     }
 
     @Override
